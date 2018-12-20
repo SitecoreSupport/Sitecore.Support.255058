2de421abec89e4187ceffeef9b08c8d6a287735f
Dmitry Kolinchuk <dmitry.kolinchuk@sitecore.com>
2018-12-20 13:31:39 +0200

    Make the RunSyncProcess processor to use a date of the created item

--- src/Sitecore.Support.255058/RunSyncProcess.cs
+++ src/Sitecore.Support.255058/RunSyncProcess.cs
@@ -2,6 +2,7 @@
 using Sitecore.Buckets.Pipelines.BucketOperations.SyncBucket;
 using Sitecore.Collections;
 using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
 using Sitecore.SecurityModel;
 using System;
 using System.Linq;
@@ -10,6 +11,31 @@ namespace Sitecore.Support.Buckets.Pipelines.BucketOperations.SyncBucket
 {
   public class RunSyncProcess : SyncBucketProcessor
   {
+    protected class SyncOperationContext
+    {
+      public int MoveOperationCount
+      {
+        get;
+        protected set;
+      }
+
+      public Item Root
+      {
+        get;
+      }
+
+      public SyncOperationContext(Item root)
+      {
+        Assert.ArgumentNotNull(root, "root");
+        Root = root;
+      }
+
+      public virtual void IncrementMoveOperations()
+      {
+        MoveOperationCount++;
+      }
+    }
+
     public override void Process(SyncBucketArgs args)
     {
       if (args != null && !args.BucketSynced)
@@ -31,29 +57,42 @@ namespace Sitecore.Support.Buckets.Pipelines.BucketOperations.SyncBucket
       {
         return false;
       }
+      SyncOperationContext syncOperationContext = new SyncOperationContext(item);
       foreach (Item child in item.GetChildren(ChildListOptions.SkipSorting))
       {
-        SyncRec(item, child);
+        SyncRec(child, syncOperationContext);
       }
+      Log.Debug($"Bucket Sync has performed {syncOperationContext.MoveOperationCount} moves for item '{AuditFormatter.FormatItem(item)}' ");
+      UpdateOperationContext(args, syncOperationContext);
       return true;
     }
 
-    private void SyncRec(Item root, Item current)
+    protected virtual void UpdateOperationContext(SyncBucketArgs args, SyncOperationContext context)
+    {
+      if (args.Context != null)
+      {
+        args.Context.MoveOperationCount = context.MoveOperationCount;
+      }
+    }
+
+    private void SyncRec(Item current, SyncOperationContext operationContext)
     {
       foreach (Item child in current.GetChildren(ChildListOptions.SkipSorting))
       {
-        SyncRec(root, child);
+        SyncRec(child, operationContext);
       }
-      if (ShouldBeMovedToRoot(current) && !IsAlreadyOnItsPlace(current, root.Paths.Path))
+      if (ShouldBeMovedToRoot(current) && !IsAlreadyOnItsPlace(current, operationContext.Root.Paths.Path))
       {
-        MoveItem(current, root);
+        MoveItem(current, operationContext.Root);
+        operationContext.IncrementMoveOperations();
       }
       if (ShouldMoveToDateFolder(current))
       {
-        //if (!IsAlreadyOnItsPlace(current, GetDestinationFolderPath(root, current.Statistics.Created, current)))
-        if (!IsAlreadyOnItsPlace(current, GetDestinationFolderPath(root, current.Created, current)))
+        //if (!IsAlreadyOnItsPlace(current, GetDestinationFolderPath(operationContext.Root, current.Statistics.Created, current)))
+        if (!IsAlreadyOnItsPlace(current, GetDestinationFolderPath(operationContext.Root, current.Created, current)))
         {
-          MoveSingleItemToDynamicFolder(root, current);
+          MoveSingleItemToDynamicFolder(operationContext.Root, current);
+          operationContext.IncrementMoveOperations();
         }
       }
       else if (ShouldDeleteInCreationOfBucket(current) && !current.GetChildren(ChildListOptions.SkipSorting).Any())
